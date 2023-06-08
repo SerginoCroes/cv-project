a2091858ec179258ad8521c86716690aa9f7fac8
w1z13 <115799235+w1z13@users.noreply.github.com>
2023-06-08 14:51:23 +0200

    slight functional and layout improvements

--- src/App.js
+++ src/App.js
@@ -4,13 +4,32 @@ import Form from "./components/Form";
 export default class App extends Component {
   constructor() {
     super();
-    this.formTypes = ['personal', 'education', 'work', 'work'];
+
+    this.state = {Education: [0], Work: [0]};
+
+
+    this.addForm = this.addForm.bind(this);
+    this.delForm = this.delForm.bind(this);
+  }
+
+  addForm(type, index) {
+    const temp = [...this.state[type]];
+    temp.push(index + 1);
+    this.setState({[type]: temp});
+  }
+
+  delForm(type, index) {
+    const temp = [...this.state[type]];
+    temp.splice(index, 1);
+    this.setState({[type]: temp});
   }
 
   render() {
     return (
       <div className="App">
-        {this.formTypes.map((formType, index) => <Form key={index} formtype={formType}/>)}        
+        <Form key='personal' formtype='Personal'/>
+        {Object.keys(this.state.Education).map((key, index) => <Form index={index} formtype='Education' add={this.addForm} del={this.delForm}/>)}
+        {Object.keys(this.state.Work).map((key, index) => <Form index={index} formtype='Work' add={this.addForm} del={this.delForm}/>)}
       </div>
     );
   }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,20 @@
 import { Component } from "react";
 import Field from "./Field";
+import Buttons from "./Buttons";
 
 export default class Form extends Component {
     constructor(props) {
         super(props);
 
         switch (this.props.formtype) {
-            case 'personal': 
-                this.fields = ['name', 'email', 'phone'];
+            case 'Personal': 
+                this.fields = ['Name', 'Email', 'Phone'];
                 break;
-            case 'education': 
-                this.fields = ['study', 'school', 'start', 'end'];
+            case 'Education': 
+                this.fields = ['Study', 'School', 'Start', 'End'];
                 break;
-            case 'work': 
-                this.fields = ['work', 'company', 'start', 'end'];
+            case 'Work': 
+                this.fields = ['Work', 'Company', 'Start', 'End'];
                 break;
             default:
                 this.fields = [];
@@ -21,14 +22,16 @@ export default class Form extends Component {
     }
 
     render() {
+
+        const {formtype, index, add, del} = this.props;
+
         return (
             <>
                 <div className="form">
                     <h3>{this.props.formtype}:</h3>
-                    {this.fields.map((field, index) => <Field key={index} fieldName={field}/>)}
-                    <input type="button" value="submit"/>
+                    {this.fields.map((field, i) => <Field key={i} fieldName={field}/>)}
                 </div>
-                {this.props.formtype !== 'personal' && <input type='button' value="add"/>}
+                {this.props.formtype !== 'Personal' && <Buttons formtype={formtype} index={index} add={add} del={del}/>}
             </>
         )        
     }
